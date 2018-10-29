74b8fbda67c3e7bb71337b82544c1f096acaeb6c
Matthew Gaddes <matthew.gaddes@gmail.com>
2018-10-29 19:47:37 +0000

    Complete video 10.

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store'); // See Store.js where this is exported!
+
 exports.homePage = (req, res) => {
     res.render('index');
 };
@@ -6,6 +9,9 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    console.log('It worked!');
+    // Note: errors are handled within the exports.catchErrors() function in errorHandlers.js - see video 11 @ 12mins for explanation
 };
